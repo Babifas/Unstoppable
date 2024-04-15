6282ecf0b477c817accbcc611928889a25349d31
BABIFAS <babifasp@gmail.com>
2024-04-15 21:57:17 +0530

    Add project files.

---
+++ Unstoppable/Model/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace Unstoppable.Model
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int UserId {  get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime CompletedAt { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted {  get; set; }
+    }
+}
